5b2434c0cf6fb10bf5e72beb12849b5e67fdefc8
kaz <kaz>
2022-12-01 12:12:03 -0500

    more

--- ddn_msg.py
+++ ddn_msg.py
@@ -1,7 +1,16 @@
+SQS_DDH_BOOT = 'DDH_BOOT'
+SQS_DDH_ERROR_BLE_HW = 'DDH_ERROR_BLE_HARDWARE'
+SQS_LOGGER_DL_OK = 'LOGGER_DOWNLOAD'
+SQS_LOGGER_ERROR_OXYGEN = 'LOGGER_ERROR_OXYGEN'
+SQS_LOGGER_MAX_ERRORS = 'LOGGER_ERRORS_MAXED_RETRIES'
+
+
 class DdnMsg:
     def __init__(self,
                  msg_ver=1):
         self.reason = None
+        self.logger_mac = None
+        self.logger_sn = None
         self.project = None
         self.vessel = None
         self.ddh_commit = None
@@ -16,7 +25,7 @@ class DdnMsg:
     def check_none(self) -> int:
         for k, v in vars(self).items():
             if not v:
-                print('variable {} is None'.format(k))
+                print('error! DdnMsg variable {} is None'.format(k))
                 return 1
         return 0
 
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='liu',
+    version='1',
+    packages=find_packages(),
+    url='https://github.com/LowellInstruments/liu.git',
+    license='MIT',
+    author='kaz',
+    author_email='',
+    description='Package smaller than MAT lib with useful functions and data types'
+)
+
